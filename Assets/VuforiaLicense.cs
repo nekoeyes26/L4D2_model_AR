1ae7f74e4129b1e45d9a29f99a258a4f52fc44f3
nekoeyes26 <nekoeyes26@gmail.com>
2024-01-08 12:15:01 +0700

    punya najib error jadi kubalikin ke UI Nawaf

---
+++ Assets/VuforiaLicense.cs
@@ -0,0 +1,14 @@
+
+            //This class is auto-generated, please do not overwrite
+            using UnityEngine.Scripting;
+
+            public class VuforiaLicense
+            {
+const string license = "QVdUbDNkdi8vLy8vQUFBQm1iVnFHS2FjZlUxUmdsZGJIZE9GZjMxMnpzaWpva2VDdU5raHVtRXhoeVVhVjF5SnAxWHlMSExxRWl1Y2ZQU3VwUGJFdGt0cis4TUc2T3hOVXBGQ0IweFVIb1pHTUpMQXVYa2ZwQ25WQzJRMHMySi9ya0NIZldIcWhlWEFtNGhGbk9HUFZmYlhlUmVrbXZNcFR3Q3NjTWxEZUlpa0tLYUliL2pwRTRPUVhhMHdoOHZGNjcwRVFoanFXQWdkWDBWTDBwNHFJSFI1b25wR28xNS8xOGRqd21yQUlmdVhFN05raUlSc2JYdWtXdkQwY3plR1kxS09BYUx0Mi9SUUkydEE2RjAzUSt0Ynp5YVRnTTN3UXNuQ3FuanRTajNQM01QQW9BaFVZVmtYSWZaaWtWRW1hdk5lVUJpVlpGb0QrYWlHeXhFN0JaU2tvdmZtSGNLVGFyZ3JLV2x3K0tZc3hhL1FHNUdFT24rcnFKNEw=";
+
+                [Preserve]
+                static string GetLicenseKey()
+                {
+                    return license;
+                }
+            }
